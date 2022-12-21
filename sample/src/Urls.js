ce4a13331397f79c42457c60a6c1f813af65a171
websource <112328498+farzeena123@users.noreply.github.com>
2022-12-21 12:34:35 +0300

    Delete sample directory

--- sample/src/Urls.js
+++
@@ -1,6 +0,0 @@
-import {API_KEY} from './Constants/Constants'
-export const originals=`https://api.themoviedb.org/3/trending/all/week?api_key=${API_KEY}&language=en-US`
-export const action=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=28`
-export const comedy=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=35`
-export const horror=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=27`
-export const romance=`https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&with_genres=10749`
